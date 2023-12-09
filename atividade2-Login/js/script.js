f9167f13097894c6d191b5a870bc67a387c77a38
nildeterocha <nildete.rocha@gmail.com>
2023-12-09 00:57:30 -0300

    Atividade avaliativa
    Atualização da atividade avaliativa, construindo Js.

--- atividade2-Login/js/script.js
+++ atividade2-Login/js/script.js
@@ -1,6 +1,29 @@
 var btnConfirmarCompra = $("#btnCalcular");
+var valorProduto = document.querySelector("#precoBolo");
+var totalProduto
+var quantidadeProduto =document.querySelector("#qtdBolo");
+var valorProdutoTexto = valorProduto.innerText || valorProduto.textContent;
+var valorProdutoFloat = parseFloat(valorProdutoTexto)
 
-btncalcular.on("click", function(){
+btnConfirmarCompra.on("click", function(){
+    totalProduto = valorProdutoFloat * quantidadeProduto.value;
+    console.log(totalProduto);
+    console.log();
+
+    var resposta = document.querySelector("#resposta");
+    resposta.innerText = totalProduto;
+
+});
+
+// PAREI AQUI PARA BAIXO
+
+var btnRealizarPagamento = $("#btnPagamento");
+
+
+
+btnRealizarPagamento.on("click", function(){
     alert();
+    
+});
 
-})
+
